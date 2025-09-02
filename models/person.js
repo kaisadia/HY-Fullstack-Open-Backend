ba5c50a699edf6677177c6f459346464c6c0062b
Kaisa Diakhate <kaisa.diakhate@academicworkacademy.fi>
2025-09-02 13:40:45 +0300

    added limitations to schema

--- models/person.js
+++ models/person.js
@@ -15,8 +15,16 @@ mongoose
   });
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: { type: String, minlength: 3, required: true },
+  number: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return /^\d{2,3}-\d{5,}$/.test(v);
+      },
+    },
+  },
 });
 
 //muokataan skeemaa: id oliosta stringiksi, poistetaan turhat
